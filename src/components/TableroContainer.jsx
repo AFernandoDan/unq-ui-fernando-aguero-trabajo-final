ed0ed6ca2f6f08c67ce01492268ac81151a4a97a
Fernando Agüero <afernandodan@gmail.com>
2023-12-03 21:46:15 -0300

    se añadieron keys en tablero container

--- src/components/TableroContainer.jsx
+++ src/components/TableroContainer.jsx
@@ -5,10 +5,10 @@ const TableroContainer = ({children}) => {
   return (
     <div className="tablero-container">
         <div className="numeros">
-            {[1,2,3,4,5,6,7,8,9, 10].map((numero) => <p>{numero}</p>)}
+            {[1,2,3,4,5,6,7,8,9, 10].map((numero) => <p key={numero}>{numero}</p>)}
         </div>
         <div className="letras">
-            {["A","B","C","D","E","F","G","H","I", "J"].map((letra) => <p>{letra}</p>)}
+            {["A","B","C","D","E","F","G","H","I", "J"].map((letra) => <p key={letra}>{letra}</p>)}
         </div>
         {children}
     </div>
